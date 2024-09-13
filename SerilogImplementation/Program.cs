5bb6edeba61769f0f420299dafedb39a4cfeb44e
Qasim-Akbar <qasim.akbar343@gmail.com>
2024-09-13 11:28:40 +0500

    Add project files.

---
+++ SerilogImplementation/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Information("Starting the application");
+Log.Error("Error encountered!");
+Log.Information("Shuting Down the application");
+
+Log.CloseAndFlush();
